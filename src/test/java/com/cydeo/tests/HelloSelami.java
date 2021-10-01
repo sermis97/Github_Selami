78f9b037b51858ffbd77281e277ad47dce287585
sermis97 <sermis97@gmail.com>
2021-10-01 16:21:22 +0300

    conflict 1 from Master

---
+++ src/test/java/com/cydeo/tests/HelloSelami.java
@@ -0,0 +1,9 @@
+package com.cydeo.tests;
+
+public class HelloSelami {
+    public static void main(String[] args) {
+
+        System.out.println("Hello from Selami");
+        System.out.println("conflict");
+    }
+}
