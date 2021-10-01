c1f2767b040a8b508538cea497dd7be3c83c85d0
sermis97 <sermis97@gmail.com>
2021-10-01 16:10:53 +0300

    commit 2 from Selami branch

--- src/test/java/com/cydeo/tests/Hello_Selami2.java
+++ src/test/java/com/cydeo/tests/Hello_Selami2.java
@@ -3,5 +3,6 @@ package com.cydeo.tests;
 public class Hello_Selami2 {
     public static void main(String[] args) {
         System.out.println("Hello Selami_2");
+        System.out.println("commit 2");
     }
 }
