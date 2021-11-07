564db0d976964a7214582a82e5a18d46c444363b
sermis97 <sermis97@gmail.com>
2021-11-07 21:48:43 +0300

    Switch case example

---
+++ src/test/java/SwitchCaseStatements.java
@@ -0,0 +1,18 @@
+public class SwitchCaseStatements {
+
+    public static void main(String[] args) {
+
+        String a= "FRIDAY";
+
+        switch (a){
+            case "SUNDAY":
+                System.out.println("Today is  SUNDAY"  ); break;
+            case "MONDAY":
+                System.out.println("Today is  MONDAY"  ); break;
+            case "WEDNESDAY":
+                System.out.println("Today is  WEDNESDAY "  ); break;
+            default:
+                System.out.println("I do not know the day ");
+        }
+    }
+}
